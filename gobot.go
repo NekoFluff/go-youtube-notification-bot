d49586a948b2601fd0b0f4b1422e363d3d96edf9
CrazyFluff <alexnou@gmail.com>
2021-12-17 20:21:43 -0700

    Cleanup

--- gobot.go
+++ gobot.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"encoding/xml"
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/NekoFluff/gobot/commands"
 	"github.com/NekoFluff/gobot/youtube/pubsubhub"
@@ -119,82 +114,5 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Content == "!ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-
-	if m.Content == "!gopher" {
-
-		//Call the KuteGo API and retrieve our cute Dr Who Gopher
-		response, err := http.Get(KuteGoAPIURL + "/gopher/" + "dr-who")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode == 200 {
-			_, err = s.ChannelFileSend(m.ChannelID, "dr-who.png", response.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println("Error: Can't get dr-who Gopher! :-(")
-		}
-	}
-
-	if m.Content == "!random" {
-
-		//Call the KuteGo API and retrieve a random Gopher
-		response, err := http.Get(KuteGoAPIURL + "/gopher/random/")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode == 200 {
-			_, err = s.ChannelFileSend(m.ChannelID, "random-gopher.png", response.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println("Error: Can't get random Gopher! :-(")
-		}
-	}
-
-	if m.Content == "!gophers" {
-
-		//Call the KuteGo API and display the list of available Gophers
-		response, err := http.Get(KuteGoAPIURL + "/gophers/")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode == 200 {
-			// Transform our response to a []byte
-			body, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			// Put only needed informations of the JSON document in our array of Gopher
-			var data []Gopher
-			err = json.Unmarshal(body, &data)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			// Create a string with all of the Gopher's name and a blank line as separator
-			var gophers strings.Builder
-			for _, gopher := range data {
-				gophers.WriteString(gopher.Name + "\n")
-			}
-
-			// Send a text message with the list of Gophers
-			_, err = s.ChannelMessageSend(m.ChannelID, gophers.String())
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println("Error: Can't get list of Gophers! :-(")
-		}
 	}
 }
