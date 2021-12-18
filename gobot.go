2e87dfb0ff35deef9769350509f01df96f89ef49
CrazyFluff <alexnou@gmail.com>
2021-12-18 00:01:07 -0700

    Add mongo

--- gobot.go
+++ gobot.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"encoding/xml"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -12,6 +14,10 @@ import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/dpup/gohubbub"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // Variables used for command line parameters
@@ -24,9 +30,57 @@ func init() {
 	flag.Parse()
 }
 
+type ChannelFeed struct {
+	FirstName  string
+	LastName   string
+	TopicURL   string
+	Group      string
+	Generation int
+}
+
+func loadMongo() []ChannelFeed {
+	uri := os.Getenv("MONGO_CONNECTION_URI")
+
+	if uri == "" {
+		log.Fatal("$MONGO_CONNECTION_URI must be set")
+	}
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
+
+	// ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	// defer cancel()
+	// client.Database("<db>").Collection("<collection>").InsertOne(ctx, bson.D{{"x",1}})
+
+	collection := client.Database("hololive-en").Collection("feeds")
+	cur, err := collection.Find(context.Background(), bson.D{})
+	var results []ChannelFeed
+	if err = cur.All(context.Background(), &results); err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Printf("%#v", results)
+	return results
+}
+
 func main() {
 	// Load the .env file in the current directory
 	godotenv.Load()
+
+	channelFeeds := loadMongo()
 
 	port, err := strconv.Atoi(os.Getenv("PORT"))
 
@@ -38,24 +92,31 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	topicURL := "https://www.youtube.com/xml/feeds/videos.xml?channel_id=UCPem6W8TYuoSs0cIAnkKy6Q"
-	client := gohubbub.NewClient("yt-notifier-bot.herokuapp.com", "YT Notifier")
-	client.DiscoverAndSubscribe(topicURL, func(contentType string, body []byte) {
-		// Handle update notification.
-		var feed pubsubhub.Feed
-		xmlError := xml.Unmarshal(body, &feed)
+	webpage := os.Getenv("WEBPAGE")
+	if webpage == "" {
+		log.Fatal("$WEBPAGE must be set")
+	}
 
-		if xmlError != nil {
-			log.Printf("XML Parse Error %v", xmlError)
+	for _, channelFeed := range channelFeeds {
+		topicURL := channelFeed.TopicURL
+		client := gohubbub.NewClient(webpage, "YT Notifier")
+		client.DiscoverAndSubscribe(topicURL, func(contentType string, body []byte) {
+			// Handle update notification.
+			var feed pubsubhub.Feed
+			xmlError := xml.Unmarshal(body, &feed)
 
-		} else {
-			log.Println("Feed title:", feed.Title)
-			for _, entry := range feed.Entries {
-				log.Printf("%s - %s (%s)", entry.Title, entry.Author.Name, entry.Link)
+			if xmlError != nil {
+				log.Printf("XML Parse Error %v", xmlError)
+
+			} else {
+				log.Println("Feed title:", feed.Title)
+				for _, entry := range feed.Entries {
+					log.Printf("%s - %s (%s)", entry.Title, entry.Author.Name, entry.Link)
+				}
 			}
-		}
-	})
-	client.StartAndServe("", port)
+		})
+		client.StartAndServe("", port)
+	}
 
 	// // Create a new Discord session using the provided bot token.
 	// dg, err := discordgo.New("Bot " + Token)
